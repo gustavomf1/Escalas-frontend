4b1d7ae4fc95ed667dae48e55c0ceda2c47f7692
Gustavo Martins <gustavo_mf1@hotmail.com>
2024-10-03 17:09:44 -0300

    Interceptor para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { PessoaListComponent } from './components/pessoa/pessoa-list/pessoa-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -53,7 +54,7 @@ import { ToastrModule } from 'ngx-toastr';
       progressBar: true
     })
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
